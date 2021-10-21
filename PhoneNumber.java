7f3d247fc921df823e060ce13d2459cc6b2050fa
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-21 12:23:33 +0530

    validate phone number

--- PhoneNumber.java
+++ PhoneNumber.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class EmailValidate {
+public class PhoneNumber {
     public void validate() {
         int flag = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Email address : ");
+        System.out.println("Enter Phone number : ");
         String getUserInput = sc.next();
-        Pattern pattern = Pattern.compile("^abc.?([A-Za-z0-9])?+@bridgelabz\\.co(.in)?$");
+        Pattern pattern = Pattern.compile("^(\\+?\\d{1,2}?)?(\\d{10})$");
         Matcher matcher = pattern.matcher(getUserInput);
         while (matcher.find()) {
             flag = 1;
         }
         if (flag == 1) {
-            System.out.println("valid email");
+            System.out.println("valid phone number");
         } else {
-            System.out.println("invalid email");
+            System.out.println("invalid phone number");
         }
     }
 }
--- RegexMain.java
+++ RegexMain.java
@@ -2,7 +2,7 @@ package com.company1.Day19_Regex;
 
 public class RegexMain {
     public static void main(String[] args) {
-        EmailValidate emailValidate = new EmailValidate();
-        emailValidate.validate();
+        PhoneNumber phoneNumber = new PhoneNumber();
+        phoneNumber.validate();
     }
 }
