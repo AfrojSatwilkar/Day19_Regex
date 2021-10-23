2679eafd9b75e2849cbc6008b7e482e531f98f16
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-23 23:43:53 +0530

    sample Email ids

--- RegexMain.java
+++
@@ -1,8 +0,0 @@
-package com.company1.Day19_Regex;
-
-public class RegexMain {
-    public static void main(String[] args) {
-        PhoneNumber phoneNumber = new PhoneNumber();
-        phoneNumber.validate();
-    }
-}
